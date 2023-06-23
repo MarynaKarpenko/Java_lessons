486bff3a4a8aeb148d2ab59245d87c2cdbbcbd4d
marina <mariina.karpenko@gmail.com>
2023-06-23 12:49:09 +0200

    new

---
+++ Lesson2/src/main/java/org/telran/Task2.java
@@ -0,0 +1,11 @@
+package org.telran;
+
+public class Task2 {
+    public int count = 0;
+
+    public static void main(String[] args) {
+    }
+    public void example() {
+        String myName = "Ivan";
+    }
+}
